e65af2560c820db6f7e924c10a7f616e56188205
Cynthia Zheng Gu <czhenggu@czhenggu-ld1.linkedin.biz>
2015-05-20 17:25:11 -0700

    add several configuration options for tsqa

---
+++ tsqa/conf_plugin.py
@@ -0,0 +1,24 @@
+import logging
+import os
+
+from nose.plugins import Plugin
+
+log = logging.getLogger('nose.plugins.ConfPlugin')
+
+class ConfPlugin(Plugin):
+
+    name = 'conf-plugin'
+    def options(self, parser, env=os.environ):
+        """Register commandline options.
+        """
+        Plugin.options(self, parser, env)
+        parser.add_option('--keep-tmp', action='store_true', dest='keep_tmp',
+                          help="Keep tmp files after running successfully")
+        parser.add_option('--sleep-in-sec', type='int',
+                          dest='sleep_in_sec',
+                          help='Sleep time before ATS start and after ATS stop to allow enough time for async tests')
+        parser.add_option('--standalone', action='store_true',
+                          dest='stand_alone',
+                          help="Allow standalone test without auto deploying ATS")
+
+
